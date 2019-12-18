5745155f3417d7c82ff21b5c6e79682ee84a6cd2
龚佳民 <2328367478@qq.com>
2019-12-18 09:41:31 +0800

    v1.3.1小修改

--- src/main/java/com/bookmarketsys/databasejob/controller/ShoppingCartController.java
+++ src/main/java/com/bookmarketsys/databasejob/controller/ShoppingCartController.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
